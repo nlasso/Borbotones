a64ad2e81425b349324889cc552240bcd3c883d0
unknown <guido@NOTEBOOKGUIDO.(none)>
2012-05-08 19:58:01 -0300

    terminando mod atleta

--- TP 2/Atleta.hs
+++ TP 2/Atleta.hs
@@ -6,7 +6,7 @@ import Tipos
 data Atleta = A String Sexo Int Pais Int [(Deporte, Int)] deriving (Show)
 
 nuevoA:: String -> Sexo -> Int -> Pais -> Int -> Atleta
-nuevoA nom s a nac cia = A (nom s a nac cia [])
+nuevoA nom s a nac cia = (A nom s a nac cia [])
 
 nombreA :: Atleta -> String
 nombreA (A n _ _ _ _ _ ) = n 
@@ -24,13 +24,20 @@ ciaNumberA:: Atleta -> Int
 ciaNumberA (A _ _ _ _ c _ ) = c
 
 deportesA:: Atleta -> [Deporte]
-deportesA (A _ _ _ _ _ [d]) = [d]
+deportesA (A _ _ _ _ _ [(d,_)]) = [d]
 
 capacidadA:: Atleta -> Deporte -> Int
-capacidadA (A _ _ _ _ _ (r,d)) = cantdecapacidad r d
+capacidadA (A n s r p c ((x,xs):xss)) d
+	| x == d = xs
+	| x /= d = capacidadA (A n s r p c xss) d
 
 entrenarDeporteA:: Atleta -> Deporte -> Int -> Atleta
-entrenarDeporteA (A n s a p c) = A n s a p c 
+entrenarDeporteA (A n s a p c (xss)) d cap = (A n s a p c (deportesFinal d xss cap))
 
+deportesFinal:: Deporte -> [(Deporte,Int)] -> Int -> [(Deporte,Int)]
+deportesFinal d [] cap = [(d,cap)] 
+deportesFinal d ((x,xs):xss) cap
+	| d == x = error "El deporte ya pertenece a la lista"
+	| d /= x = (x,xs):deportesFinal d xss cap
 
-
+	
