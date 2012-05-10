22c7d860c1df2d35839713f65e40cab591ce3902
unknown <guido@NOTEBOOKGUIDO.(none)>
2012-05-10 01:57:45 -0300

    terminado nuevaC hecho participantesC subio GT

--- TP 2/Competencia.hs
+++ TP 2/Competencia.hs
@@ -1,4 +1,4 @@
-module Competencia (Competencia, nuevaC, categoriaC)
+module Competencia (Competencia, nuevaC, categoriaC,participantesC)
 
 where
 import Tipos
@@ -9,29 +9,33 @@ data Competencia = C Categoria
 
 nuevaC :: Deporte -> Sexo -> [Atleta] -> Competencia
 nuevaC d s [a]
-	| deporteIgual d [a] && todosDistintos [a] = C (d,s) 
+	| deporteIgual d [a] && sexoIgual s [a] && todosDistintos [a] = C (d,s) 
 	| deporteIgual d [a] == False = error "Los participantes no pertenecen a la categoria"
 	| todosDistintos [a] == False = error "Atletas repetidos"
 
-
+sexoIgual :: Sexo -> [Atleta] -> Bool
+sexoIgual s [] = True
+sexoIgual s (x:xs)
+	| not(s == (sexoA x))= False
+	| s == (sexoA x) = sexoIgual s xs
 	
 deporteIgual :: Eq Deporte => Deporte -> [Atleta] -> Bool
-deporteIgual [] d = True
+deporteIgual d [] = True
 deporteIgual d (x:xs)
 	| not(d `elem` (deportesA x)) = False
 	| d `elem` (deportesA x) = deporteIgual d xs
-
-
-
 
 todosDistintos:: [Atleta] -> Bool
 todosDistintos [] = True
 todosDistintos [x] = True
 todosDistintos (x:y:xs) = ciaNumberA x /= ciaNumberA y && todosDistintos (y:xs)
 
-
-	
 categoriaC:: Competencia -> (Deporte,Sexo)
 categoriaC (C (d,s)) = (d,s)
+
+participantesC:: Competencia ->[Atleta]
+participantesC ( C _ ) = []
+participantesC ( Participar a c ) = (a : participantesC c)
+participantesC ( Finalizar _ _ c ) = participantesC c
 
 
