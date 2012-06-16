d7e2f91b0c4a92d2738bb5476b62482939da7d00
Nicolas Lasso <lasso.nico@gmail.com>
2012-06-16 17:23:47 -0300

    queen

--- TP3/ojota.cpp
+++ TP3/ojota.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 Atleta atletaProdigio(const JJOO & j)
 {
-<<<<<<< HEAD
     int i = 0;
     Atleta a;
     if(j.competenciasFinalizadasConOroEnPodio().longitud()>0)
@@ -24,12 +23,6 @@ Atleta atletaProdigio(const JJOO & j)
             i++;
     }
     return a;
-=======
-    Lista<Competencia> competenciasConOro = j.competenciasFinalizadasConOroEnPodio();
-    Lista<Atleta> misAtletasOro = Lista<Atleta>();
-
-    return Atleta();
->>>>>>> 39fb5224a7fd39d4c76776a420d358d1b94639b5
 }
 
 int main()
