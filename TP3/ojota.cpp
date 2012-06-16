87c0131537b2eadff5ab66c52a6bbe646d83b65f
unknown <guido@NOTEBOOKGUIDO.(none)>
2012-06-16 17:09:43 -0300

    sube atleta prodigio

--- TP3/ojota.cpp
+++ TP3/ojota.cpp
@@ -7,7 +7,21 @@ using namespace std;
 
 Atleta atletaProdigio(const JJOO & j)
 {
-    return Atleta();
+    int i = 0;
+    Atleta a;
+    if(j.competenciasFinalizadasConOroEnPodio().longitud()>0)
+        {
+            a = j.competenciasFinalizadasConOroEnPodio().iesimo(0).ranking().iesimo(0);
+        }
+    while(i< j.competenciasFinalizadasConOroEnPodio().longitud())
+    {
+            if(a.anioNacimiento()< j.competenciasFinalizadasConOroEnPodio().iesimo(i).ranking().iesimo(0).anioNacimiento())
+            {
+                a = j.competenciasFinalizadasConOroEnPodio().iesimo(i).ranking().iesimo(0);
+            }
+            i++;
+    }
+    return a;
 }
 
 int main()
